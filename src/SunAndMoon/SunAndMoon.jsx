d9d5175cb395e26d951aea18d32c34dad7b7b9b7
Shohreh Ghafoormohseni <shohreh.mohseni@gmail.com>
2024-01-22 16:22:12 -0800

    L2: Complete

---
+++ src/SunAndMoon/SunAndMoon.jsx
@@ -0,0 +1,48 @@
+import Styled from "styled-components";
+import { useState } from "react";
+
+import sunImg from "./images/sun.png";
+import moonImg from "./images/moon.png";
+
+const SunAndMoon = () => {
+    const [imgSrc, imgSrcUpdate] = useState(moonImg);
+
+    const changeToSun = () => {
+        imgSrcUpdate(sunImg);
+    };
+    const changeToMoon = () => {
+        imgSrcUpdate(moonImg);
+    };
+
+    // HTML Layer
+    return (
+        <SunAndMoonStyled>
+            <h2>Sun And Moon</h2>
+            <img
+                src={imgSrc}
+                onMouseEnter={changeToSun}
+                onMouseLeave={changeToMoon}
+            />
+        </SunAndMoonStyled>
+    );
+};
+
+export default SunAndMoon;
+
+// css Layer
+const SunAndMoonStyled = Styled.div`
+    text-align: center;
+
+    h2 {
+        font-size: 30px;
+        color: teal;
+    }
+
+    img {
+        display: block;
+        margin: 30px auto;
+        padding: 20px;
+        border: solid 20px teal;
+        background-color: #004e4e;
+    }
+`;
---
+++ src/SunAndMoon/images/SunAndMoon.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const SunAndMoon = () => {
+    return <SunAndMoonStyled>Sun And Moon</SunAndMoonStyled>;
+};
+
+export default SunAndMoon;
+
+// CSS Layer
+const SunAndMoonStyled = styled.div`
+    text-align: center;
+`;
