04d20bfd6e0b0eb39ca7a5097c3dbb9d8cfdddc4
Shohreh Ghafoormohseni <shohreh.mohseni@gmail.com>
2024-02-12 14:23:55 -0800

    L05 Complete

---
+++ src/Common/PagesLayout/Header/Header.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+//Media Query
+import { useMediaQuery } from "@/Common/useMediaQuery";
+
+//components
+import MediumLarge from "./MediumLarge";
+import Small from "./Small";
+
+const Header = () => {
+    const { isMediumAndUp } = useMediaQuery();
+
+    return (
+        <HeaderStyled>
+            {isMediumAndUp ? <MediumLarge /> : <Small />}
+        </HeaderStyled>
+    );
+};
+
+export default Header;
+
+const HeaderStyled = styled.header``;
